07881226c2b2d56cfd6780dc55e724348e7340c3
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-10-07 17:51:01 +0530

    Initial Commit

---
+++ src/components/blog-post-footer.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+const BlogPostFooter = () => {
+  // const data = useStaticQuery(graphql`
+  //   query BioQuery {
+  //     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+  //       childImageSharp {
+  //         fixed(width: 50, height: 50) {
+  //           ...GatsbyImageSharpFixed
+  //         }
+  //       }
+  //     }
+  //     site {
+  //       siteMetadata {
+  //         author
+  //         social {
+  //           twitter
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // const { author, social } = data.site.siteMetadata
+  return (
+    <div
+      style={{
+        display: `flex`,
+        marginBottom: rhythm(2.5),
+      }}
+    >
+      <p>
+        Written by <strong>Md Abu Zafar</strong>
+      </p>
+    </div>
+  )
+}
+
+export default BlogPostFooter
---
+++ src/components/layout.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>{header}</header>
+        <main>{children}</main>
+        <footer>
+          <a href="https://www.twitter.com/MdAbuZafar19" target="_blank">
+            Twitter
+          </a>
+          {` `}
+          <a href="https://www.github.com/mdaz78" target="_blank">
+            Github
+          </a>
+          {` `}
+          <a href="https://www.linkedin.com/in/mdaz78/" target="_blank">
+            LinkedIn
+          </a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
